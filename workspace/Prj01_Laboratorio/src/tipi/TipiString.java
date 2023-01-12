f5c683945bf93bc4ea964b7acb7dbf3ec8b7f97b
maboglia <mauro.bogliaccino@gmail.com>
2023-01-12 13:57:20 +0100

    modifiche

---
+++ workspace/Prj01_Laboratorio/src/tipi/TipiString.java
@@ -0,0 +1,16 @@
+package tipi;
+
+public class TipiString {
+
+	public static void main(String[] args) {
+
+		String saluto = "ciao";
+		saluto += " mondo";
+
+		System.out.println(saluto.toUpperCase());
+		
+		
+
+	}
+
+}
