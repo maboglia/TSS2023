8c41472fe2ceb0fa25163728f716749a0264c0e6
maboglia <mauro.bogliaccino@gmail.com>
2023-01-25 12:49:31 +0100

    agg

--- workspace/Prj07_RubricaMVC/src/RubricaDemo.java
+++ workspace/Prj07_RubricaMVC/src/RubricaDemo.java
@@ -1,24 +1,46 @@
 import controller.Rubrica;
 import main.MioScanner;
+import model.Contatto;
 
 public class RubricaDemo {
 
 	public static void main(String[] args) {
-		
-		Rubrica agendaRossa = new Rubrica(10);
-		
+
+		boolean gira = true;
+		Rubrica agendaRossa = new Rubrica(1);
 		MioScanner msc = new MioScanner();
-		
-		String nome = msc.getParola("Nome del contatto");
-		
-		String numero = msc.getParola("Numero di telefono");
-		
-		agendaRossa.add(nome, numero);
-		
-		System.out.print( agendaRossa.getContatti()[0].mostraInfo() );
-		
-		
 
+		while (gira) {
+			System.out.println("1) aggiungi contatto");
+			System.out.println("2) mostra contatti");
+			System.out.println("9) esci");
+
+			int scelta = msc.getIntero("Fai una scelta");
+
+			switch (scelta) {
+			case 1:
+				String nome = msc.getParola("Nome del contatto");
+				String numero = msc.getParola("Numero di telefono");
+				agendaRossa.add(nome, numero);
+				break;
+			case 2:
+				for (Contatto c : agendaRossa.getContatti()) {
+					if (c != null) {
+						System.out.println(c.mostraInfo());
+					} else {
+						System.out.println("---VUOTO---");
+					}
+				}
+				break;
+			case 9:
+				gira = false;
+				break;
+			default:
+				System.out.println("opzione non disponibile");
+				break;
+			}
+		}
+		System.out.println("Programma terminato");
+//		System.out.print( agendaRossa.getContatti()[0].mostraInfo() );
 	}
-
 }
