2ba40240b8902d8d462b7d5d72e2624e59f35963
maboglia <>
2023-04-14 10:37:15 +0200

    aggiornamento index

--- workspace/Prj01_Laboratorio/src/thread/ProvaThread.java
+++ workspace/Prj01_Laboratorio/src/thread/ProvaThread.java
@@ -4,14 +4,7 @@ public class ProvaThread {
 
 	public static void main(String[] args) {
 
-		Runnable r = new Runnable() {
-			
-			@Override
-			public void run() {
-				System.out.println("Sono un runner :)");
-				
-			}
-		};
+		Runnable r = () -> System.out.println("Sono un runner :)");
 		
 		r.run();
 		
