55d613c935e7872c5ca24706b6ce95e510b4b2c6
maboglia <>
2023-05-12 13:54:22 +0200

    aggiornamentox

--- workspaceSpring/viaggi-admin/src/main/java/it/bogliaccino/ViaggiAdminApplication.java
+++ workspaceSpring/viaggi-admin/src/main/java/it/bogliaccino/ViaggiAdminApplication.java
@@ -2,10 +2,20 @@ package it.bogliaccino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
+@RequestMapping("")
 public class ViaggiAdminApplication {
 
+	@GetMapping("test")
+	String home() {
+		return "prova";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ViaggiAdminApplication.class, args);
 	}
