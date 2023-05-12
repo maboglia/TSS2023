8cc09cf07aa2fd03f0b4322d2fabd156fddde5f0
maboglia <>
2023-05-12 11:21:33 +0200

    aggiornamentox

---
+++ workspaceSpring/maven-servlet/src/main/java/MiaServlet.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class MiaServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("application/json");
+		response.getWriter().println("{ \"patate\": 125 }");
+	
+	
+	}
+	
+	
+
+}
